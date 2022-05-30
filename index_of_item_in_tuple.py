a362fc972c07106a4cc7d903a93418af3718a548
SUNIL KUMAR <91265823+sunilthakurr@users.noreply.github.com>
2022-05-30 20:07:05 +0530

    program to find the index of an item of a tuple

---
+++ index_of_item_in_tuple.py
@@ -0,0 +1,14 @@
+
+tuplex = tuple("index tuple")
+print(tuplex)
+
+index = tuplex.index("p")
+print(index)
+
+index = tuplex.index("p", 5)
+print(index)
+
+index = tuplex.index("e", 3, 6)
+print(index)
+
+index = tuplex.index("y")
